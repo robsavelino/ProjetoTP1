f7166ecebd8e7a489c860eee89809edb65c4aba4
Lugan Thierry <luganthierry@hotmail.com>
2022-08-25 17:57:04 -0300

    Commit com a tela do usuário

---
+++ ProjetoTP1/FrmUserScreen.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Projeto
+{
+    partial class FrmUserScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnGames = new System.Windows.Forms.Button();
+            this.BtnFriends = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnGames
+            // 
+            this.BtnGames.Location = new System.Drawing.Point(523, 115);
+            this.BtnGames.Name = "BtnGames";
+            this.BtnGames.Size = new System.Drawing.Size(170, 44);
+            this.BtnGames.TabIndex = 0;
+            this.BtnGames.Text = "Jogos";
+            this.BtnGames.UseVisualStyleBackColor = true;
+            // 
+            // BtnFriends
+            // 
+            this.BtnFriends.Location = new System.Drawing.Point(523, 250);
+            this.BtnFriends.Name = "BtnFriends";
+            this.BtnFriends.Size = new System.Drawing.Size(170, 44);
+            this.BtnFriends.TabIndex = 1;
+            this.BtnFriends.Text = "Amigos";
+            this.BtnFriends.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = global::Projeto.Properties.Resources.DefaultUser;
+            this.pictureBox1.InitialImage = global::Projeto.Properties.Resources.DefaultUser;
+            this.pictureBox1.Location = new System.Drawing.Point(49, 59);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(111, 100);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FrmUserScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.BtnFriends);
+            this.Controls.Add(this.BtnGames);
+            this.Name = "FrmUserScreen";
+            this.Text = "FrmUserScreen";
+            this.Load += new System.EventHandler(this.FrmUserScreen_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button BtnGames;
+        private Button BtnFriends;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ ProjetoTP1/FrmUserScreen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto
+{
+    public partial class FrmUserScreen : Form
+    {
+        public FrmUserScreen(List<User> Users)
+        {
+            InitializeComponent();
+        }
+
+        private void FrmUserScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
